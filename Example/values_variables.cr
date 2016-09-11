5bcb1a431b08ce000c468d6487efaa3a62903261
tolgahanuzun <mail@tolgahanuzun.com>
2016-09-11 22:42:28 +0300

    Values Variables

---
+++ Example/values_variables.cr
@@ -0,0 +1,26 @@
+string_values = "crystal \nlang \nexample code "
+puts string_values
+
+int_values = 1 + 1
+puts "1+1 = #{int_values}"
+
+float_values = 7.0 / 3.0
+puts "7.0/3.0 = #{float_values}"
+
+boolen_values = true && false
+puts boolen_values
+
+boolen_values = true || false
+puts boolen_values
+
+boolen_values = !true
+puts boolen_values
+
+test_type = "I'm a string variables!"
+puts test_type
+
+test_type = 5*10
+puts "Ups!! Now my type is a int values! #{test_type}"
+
+a, b = "'a' variables is values", "'b' variables is values"
+puts a, b
